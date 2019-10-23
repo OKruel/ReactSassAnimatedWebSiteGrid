d8b7feaf0463ed44318b619126adabaef5ef5897
olavokruel <olavokruel@gmail.com>
2019-10-23 18:16:20 -0300

    finished styling story section

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const StoryContent = props => (
-    <div className='story__content'>StoryContent</div>
+    <div className='story__content'>
+        <h3 className='heading-3 mb-sm'>Happy costumers</h3>
+        <h2 className='heading-2 heading-2--dark mb-md'>&ldquo;The best decision of our lives&rdquo;</h2>
+        <p className='story__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!</p>
+        <button className='btn'>Find your home</button>
+    </div>
 )
 
 export default StoryContent;
--- src/components/StoryPictures.js
+++ src/components/StoryPictures.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import story1 from '../assets/img/story-1.jpeg';
+import story2 from '../assets/img/story-2.jpeg';
 
 const StoryPictures = props => (
-    <div className='story__pictures'>StoryPictures</div>
+    <div className='story__pictures'>
+        <img src={story1} alt='Couple with a new house' className='story__img--1'></img>
+        <img src={story2} alt='new house' className='story__img--2'></img>
+    </div>
 )
 
 export default StoryPictures;
